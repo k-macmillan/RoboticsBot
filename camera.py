199ff8c5d88cb5814b76d253f558c56f0234f802
Kyle MacMillan <macattackftw@gmail.com>
2018-09-03 17:00:44 -0600

    Added method to adjust saturation and brightness to try and make it easier to find yellow, worked soso.

--- camera.py
+++ camera.py
@@ -7,12 +7,26 @@ class Camera():
     def __init__(self):
         self.vid_capture = cv.VideoCapture(1)
         self.lf = linef.LineFollow()
+        self.frame = 0
+
+    def adjustSaturationBrightness(self):
+        hsvImg = cv.cvtColor(self.frame,cv.COLOR_BGR2HSV)
+
+        #multiple by a factor to change the saturation
+        hsvImg[...,1] = hsvImg[...,1]*2.8
+
+        #multiple by a factor of less than 1 to reduce the brightness 
+        hsvImg[...,2] = hsvImg[...,2]*0.5
+
+        self.frame = cv.cvtColor(hsvImg,cv.COLOR_HSV2BGR)
+
 
     def loop(self):
         while (True):
-            ret, frame = self.vid_capture.read()
-            self.lf.UpdateAll(frame)
-            cv.imshow('Untouched Frame',frame)
+            ret, self.frame = self.vid_capture.read()
+            self.adjustSaturationBrightness()
+            self.lf.UpdateAll(self.frame)
+            cv.imshow('Untouched Frame',self.frame)
             cv.imshow('Black Detection',self.lf.hsv_mask.mask)
             cv.imshow('Yellow Detection', self.lf.stop_mask.mask)
             if cv.waitKey(1) & 0xFF == ord('q'):
@@ -21,6 +35,7 @@ class Camera():
         # When everything done, release the capture
         self.vid_capture.release()
         cv.destroyAllWindows()
+
 
 
 if __name__ == "__main__":
