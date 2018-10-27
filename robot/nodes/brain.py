634187cbe31852f75e010297dd037ea183a5ef34
Kyle MacMillan <macattackftw@gmail.com>
2018-10-27 14:59:49 -0600

    Started to deal with the robot states that can happen while in the Brain class.

--- robot/nodes/brain.py
+++ robot/nodes/brain.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 from __future__ import print_function
 import rospy as ros
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, Int32
 from geometry_msgs.msg import Twist, Vector3
 
 from robot.nodes import Node
@@ -21,19 +21,58 @@ class Brain(Node):
                                    queue_size=10)
 
         ros.Subscriber(CAM_CENTER_DIST, Float32, self.__generateTwist)
+        ros.Subscriber(CAM_OBJECT, Int32, self.__handleObject)
+
+    def __handleObject(self, msg):
+        """Reads in object"""
+        self.__determineState()
+
+    def __determineState(self):
+        """Sets state based on what objects are seen."""
+        pass
 
     def __generateTwist(self, msg):
         """Processes the distance from center and publish."""
-        twist = Twist()
+        if self.state == State.START:
+            twist = self.__startTwist(msg)
+        elif self.state == State.ROAD:
+            twist = self.__roadTwist(msg)
+        elif self.state == State.ROAD:
+            twist = self.__startTwist(msg)
+        elif self.state == State.P_LOT:
+            twist = self.__parkinglotTwist(msg)
+        else:
+            twist = None
+
+        
         # Set twist message here:
         twist.linear = Vector3(0, 0, 0)
         twist.angular = Vector3(0, 0, 0)
 
-        if verbose:
-            print('Linear: (%f, %f, %f)' %(twist.linear.x,
-                                           twist.linear.y
-                                           twist.linear.z))
-            print('Angular: (%f, %f, %f)\n' %(twist.angular.x,
-                                              twist.angular.y
-                                              twist.angular.z))
-        self.twist.publish(twist)
+        if twist is not None:
+            if verbose:
+                print('Linear: (%f, %f, %f)' %(twist.linear.x,
+                                               twist.linear.y
+                                               twist.linear.z))
+                print('Angular: (%f, %f, %f)\n' %(twist.angular.x,
+                                                  twist.angular.y
+                                                  twist.angular.z))
+            self.twist.publish(twist)
+
+    def __startTwist(self, msg):
+        twist = Twist()
+        twist.linear = Vector3(0, 0, 0)
+        twist.angular = Vector3(0, 0, 0)
+        return twist
+
+    def __roadTwist(self, msg):
+        twist = Twist()
+        twist.linear = Vector3(0, 0, 0)
+        twist.angular = Vector3(0, 0, 0)
+        return twist
+
+    def __parkinglotTwist(self, msg):
+        twist = Twist()
+        twist.linear = Vector3(0, 0, 0)
+        twist.angular = Vector3(0, 0, 0)
+        return twist
