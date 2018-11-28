700ad5a14f10e3c8888f085ad25b23d0451c7fe7
Kyle MacMillan <macattackftw@gmail.com>
2018-11-28 00:33:30 -0700

    Updated for PEP8 standard.

--- robot/common.py
+++ robot/common.py
@@ -17,6 +17,7 @@ LANE_CENTROID = '/geekbot/lane_centroid'
 # Notification that we've reached a point of interest.
 POINT_OF_INTEREST = '/geekbot/encounter_interest'
 
+
 class State(Enum):
     """Possible robot states.
 
