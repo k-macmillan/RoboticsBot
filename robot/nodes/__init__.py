e97792b6e4e5eeffe62f10224ffa73dbcaa6d706
Austin Gill <notgnoshi@gmail.com>
2018-11-29 19:22:36 -0700

    Reorder imports to fix dependency hell

--- robot/nodes/__init__.py
+++ robot/nodes/__init__.py
@@ -1,7 +1,7 @@
 """A collection of ROS nodes for the robot."""
 
+from .drive_line import DriveLine
 from .node_manager import Node, NodeManager
 from .ir_sensor import IrPlotter, IrSpammer
 from .brain import Brain
 from .wheels import Wheels
-from .drive_line import DriveLine
