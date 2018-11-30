6fabb96d5a7899b72500a81d11a487dc474fb573
Austin Gill <notgnoshi@gmail.com>
2018-11-29 20:50:54 -0700

    Tweak PID gains. IT ACTUALLY WORKS.

--- robot/nodes/drive_line.py
+++ robot/nodes/drive_line.py
@@ -5,9 +5,9 @@ from __future__ import division, print_function
 class DriveLine(object):
     """Simple singleton to handle wheel speeds while line following."""
 
-    Kp = 1
-    Ki = 0.001  # Not sure we want this
-    Kd = 1.2
+    Kp = 0.4
+    Ki = 0
+    Kd = 0.1
 
     def __init__(self, r, L, verbose=False):
         """Initialize variables for this singleton.
