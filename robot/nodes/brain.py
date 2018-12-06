e6151b9aec7e3ae6e31db31ed4c3e1ea41c8ed2f
Kyle MacMillan <macattackftw@gmail.com>
2018-12-05 23:11:49 -0700

    Removed double underscores.
    Team member requested removal of double underscores, no problem.

--- robot/nodes/brain.py
+++ robot/nodes/brain.py
@@ -46,11 +46,11 @@ class Brain(Node):
 
     def init_node(self):
         """Perform custom Node initialization."""
-        # ros.Subscriber(TOPIC['LANE_CENTROID'], Float32, self.__correctPath)
-        ros.Subscriber(TOPIC['GOAL_CENTROID'], Float32, self.__topicGoal)
+        # ros.Subscriber(TOPIC['LANE_CENTROID'], Float32, self.correctPath)
+        ros.Subscriber(TOPIC['GOAL_CENTROID'], Float32, self.topicGoal)
         ros.Subscriber(TOPIC['POINT_OF_INTEREST'],
                        String,
-                       self.__topicPOI)
+                       self.topicPOI)
 
     def transition(self, state):
         """Transition the robot's state to that given.
@@ -65,10 +65,10 @@ class Brain(Node):
         msg.data = self.state.value
         self.state_pub.publish(msg)
 
-    def __topicGoal(self, msg):
+    def topicGoal(self, msg):
         self.goal_error = msg.data
 
-    def __topicPOI(self, msg):
+    def topicPOI(self, msg):
         """Handle a Point of Interest notification.
 
         This determines the robot's current state.
@@ -80,7 +80,7 @@ class Brain(Node):
                 self.state == State.ON_PATH and self.rl_count < 2:
             self.transition(State.STOPPING)
             if self.rl_count == 1:
-                w1, w2 = self.__stateHandler(0)
+                w1, w2 = self.stateHandler(0)
                 # We only want to change self.w if not in CANCER
                 if self.state != State.CANCER:
                     self.w1 = w1
@@ -98,42 +98,42 @@ class Brain(Node):
         elif msg.data == POI['NO_EXIT_LOT']:
             self.exit = False
 
-    def __stateTimer(self):
+    def stateTimer(self):
         if self.state_timer is None:
             self.state_timer = ros.Timer(
-                ros.Duration(secs=0.01), self.__stateHandler2)
+                ros.Duration(secs=0.01), self.stateHandler2)
 
-    def __stateHandler2(self):
+    def stateHandler2(self):
         if self.state == State.CANCER:
-            self.__cancerState()
+            self.cancerState()
         elif self.state == State.SPIN:
-            self.__spinState()
+            self.spinState()
         elif self.state == State.TURN:
-            self.__turnState()
+            self.turnState()
         elif self.state == State.MTG:
-            self.__mtgState()
+            self.mtgState()
         elif self.state == State.GRAPH:
             pass
 
-    def __cancerState(self):
+    def cancerState(self):
         if self.obstacle:
             self.transition(State.SPIN)
         elif self.exit:
             self.transition(State.MTG)
         else:
-            self.__setWheels(self.base_sp, self.base_sp)
+            self.setWheels(self.base_sp, self.base_sp)
 
-    def __spinState(self):
+    def spinState(self):
         pass
 
-    def __turnState(self):
+    def turnState(self):
         pass
 
-    def __mtgState(self):
+    def mtgState(self):
 
         pass
 
-    def __setWheels(self, w1=None, w2=None):
+    def setWheels(self, w1=None, w2=None):
         if w1 is None or w2 is None:
             w1 = self.w1
             w2 = self.w2
@@ -141,20 +141,20 @@ class Brain(Node):
         wheels.data = [w1, w2]
         self.wheel_speeds.publish(wheels)
 
-    def __correctPath(self, msg):
+    def correctPath(self, msg):
         """Process the lane centroid and control x and theta velocities.
 
         :param msg: The lane centroid message.
         :type msg: std_msgs.msg.Float32
         """
-        self.w1, self.w2 = self.__stateHandler(msg.data)
+        self.w1, self.w2 = self.stateHandler(msg.data)
 
         wheels = Float32MultiArray()
         wheels.data = [self.w1, self.w2]
         self.wheel_speeds.publish(wheels)
 
 
-    def __stateHandler(self, error):
+    def stateHandler(self, error):
         """Handle the current state of our robot.
 
         Returns wheel velocities. Unfortunately this is spaghetti code. It was
@@ -164,16 +164,16 @@ class Brain(Node):
             self.transition(State.ON_PATH)
             return self.DL.calcWheelSpeeds(self.base_sp, self.base_sp, 0.0)
         elif self.state == State.STOPPING or self.state == State.STOPPED:
-            return self.__stopStateHandler(error)
+            return self.stopStateHandler(error)
         elif self.state == State.ON_PATH:
             # Adjust based on camera (error)
             return self.DL.calcWheelSpeeds(self.w1, self.w2, error)
         elif self.state == State.CANCER:
             # Begin state timer that we will use from here on out
-            self.__stateTimer()
+            self.stateTimer()
             return 0.0, 0.0
 
-    def __stopStateHandler(self, error):
+    def stopStateHandler(self, error):
         """Handle the stopped state of our robot."""
         if self.state == State.STOPPING:
             sleep(1.0)
@@ -193,12 +193,12 @@ class Brain(Node):
         # elif self.state == obstacle
         # elif self.state == graph
 
-    def __obstacleAvoidance(self):
+    def obstacleAvoidance(self):
         if not self.spun:
             self.spun = True
             self.last_state = self.state
             self.transition(State.SPIN)
-            self.__forceCorrectPath(0)
+            self.forceCorrectPath(0)
             return 0.0, 0.0
 
         if not self.obst_rot:
@@ -211,22 +211,22 @@ class Brain(Node):
         return self.DL.calcWheelSpeeds(self.w1 * self.multiplier,
                                        self.w2 * -self.multiplier, 0.0)
 
-    def __start_timer(self):
+    def start_timer(self):
         if self.timer is None:
             self.timer = ros.Timer(
-                ros.Duration(secs=0.01), self.__timerCallback)
+                ros.Duration(secs=0.01), self.timerCallback)
 
-    def __timerCallback(self, event):
+    def timerCallback(self, event):
         if self.timer_counter > 365:
             self.transition(State.OBSTACLE)
-            self.__timerShutdown()
+            self.timerShutdown()
         elif self.last_error < 1000.0:
             self.transition(State.CANCER)
-            self.__timerShutdown()
+            self.timerShutdown()
         else:
             self.timer_counter += 1
 
-    def __timerShutdown(self):
+    def timerShutdown(self):
         self.timer.shutdown()
         self.timer_counter = 0
         self.timer = None
