beab089cf587b2e5409902afef6e31c719c00378
Austin Gill <notgnoshi@gmail.com>
2018-12-06 17:21:45 -0700

    Add graph states et al to common constants

--- robot/common.py
+++ robot/common.py
@@ -17,10 +17,10 @@ TOPIC = {
     'LANE_CENTROID': '/geekbot/lane_centroid',
     # Notification that we've reached a point of interest.
     'POINT_OF_INTEREST': '/geekbot/encounter_interest',
-    # The gradient at the current location in the parking lot.
-    'LOT_GRADIENT': '/geekbot/cancer/gradient',
     # Goal position in the viewing window.
     'GOAL_CENTROID': '/geekbot/goal_centroid',
+    # Centroid of graph node.
+    'NODE_CENTROID': '/geekbot/node_centroid',
 }
 
 # Camera POI strings
@@ -30,28 +30,65 @@ POI = {
     'EXIT_LOT': 'exit parking lot',
     'NO_EXIT_LOT': 'no exit parking lot',
     'GRAPH_NODE': 'graph node',
+    'NO_GRAPH_NODE': 'no graph node',
     'OBSTACLE': 'obstacle',
     'NO_OBSTACLE': 'no obstacle',
+}
+
+GRAPH_PATH = {
+    0: (0,),
+    1: (0, 1),
+    2: (0, 1, 2),
+    3: (0, 1, 3),
+    4: (0, 1, 4),
+    5: (0, 1, 4, 5),
+}
+
+# Possible left turns on pruned graph.
+LEFT_TURN = {
+    (1, 4),
+    (4, 5),
+}
+
+# Possible right turns on pruned graph.
+RIGHT_TURN = {
+    (1, 3),
+}
+
+# Possible front "turns" on pruned graph.
+FORWARD = {
+    (0, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (4, 5),
 }
 
 
 class State(Enum):
     """Possible robot states.
 
-    The robot has states:
+    The robot has the following states:
 
-    START - we've just started up and are waiting to begin lane-following.
     ON_PATH - we're currently following the lane.
     STOPPING - process of stopping has begun or is in work.
-    STOPPED - currently stopped. Either at a stoplight, lot exit, or node.
+    STOPPED - currently stopped at a stoplight.
     CANCER - we're searching for the parking lot exit.
-    OBSTACLE - found an obstacle (wall or car).
-    ORIENT - we're at an intersection and are turning in place so that the road
-    is directly in front of us.
-    GRAPH - we're traversing the graph.
-    END - we're done. Queue the less-heavy drinking.
-
+    SPIN - Rotate 360 degrees every time we hit an obstacle to look for the
+    lot exit.
+    TURN - Rotate in place in a random direction until not obstructed.
+    MTG - Motion to goal.
+    GRAPH - We are at the lot exit.
+    ORIENTING - Rotation until centered on lane exiting parking lot.
+    G_ON_PATH - Following the lane out of the parking lot.
+    NODE_STOPPING - Process of stopping at a node in in progress.
+    NODE_STOPPED - Currently stopped at a node.
+    ROTATE_LEFT - Rotating left until node is in view.
+    ROTATE_RIGHT - Rotating right until node is in view.
+    FORWARD - Drive forward until at another node.
+    END - we're done. Queue the less (more?)-heavy drinking.
     """
+
     # Line follow
     ON_PATH = 1
     STOPPING = 2
@@ -65,5 +102,11 @@ class State(Enum):
 
     # Graph
     GRAPH = 8
-    ORIENT = 9
-    END = 10
+    ORIENTING = 9
+    G_ON_PATH = 10
+    NODE_STOPPING = 11
+    NODE_STOPPED = 12
+    ROTATE_LEFT = 13
+    ROTATE_RIGHT = 14
+    FORWARD = 15
+    END = 16
